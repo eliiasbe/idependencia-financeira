4d43c32039ef6cee6349fbe40c93605697fe0a68
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-23 21:32:45 -0300

    atividade

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+decimal gastosMensais, valorAInvestir;
+double dividendYield;
+Console.WriteLine(" Vivendo de dividendos\n");
+Console.Write("Gasto mensal (em R$)....: ");
+gastosMensais = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Rendimentos mensais (%)...: ");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+valorAInvestir = gastosMensais / Convert.ToDecimal(dividendYield / 100);
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investido {valorAInvestir:C}.");
